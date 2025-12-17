ee26787abc3dc77ca23ccc14f95ade08573e3096
vishwa9011 <vishu842301@gmail.com>
2025-12-17 11:45:38 +0530

    fix: ensure server opens automatically on start

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
     server: {
         port: 3000,
+        open: true,
     },
     plugins: [
         tanstackRouter({
